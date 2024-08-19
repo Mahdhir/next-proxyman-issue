91db5807fb414475a3b8d7b5af5806422e6b758c
Mahdhir <mahdhirezvi@gmail.com>
2024-08-19 20:05:42 +0530

    Added middleware example

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,12 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export async function middleware(request: NextRequest) {
+  // Doesn't show up on proxyman
   // const res = await fetch('https://jsonplaceholder.typicode.com/todos/1', {
   //   cache: 'no-cache',
   // });
+
+  // Shows up on proxyman
   const res = await fetch('http://localhost:3000/api', {
     cache: 'no-cache',
   });
@@ -14,3 +17,7 @@ export async function middleware(request: NextRequest) {
 
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/((?!_next/static|_next/image|favicon.ico|api).*)'],
+};
